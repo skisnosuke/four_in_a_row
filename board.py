b21b0c057c447b34b605a01a04074959328dcfb7
eteRnaL198 <ikuya313198@gmail.com>
2021-09-21 11:40:15 +0900

    勝利判定実装

--- board.py
+++ board.py
@@ -32,7 +32,7 @@ class Board:
         for piece in self.pieces:
             piece.draw(surface)
         
-    def get_column(self, x):
+    def _get_column(self, x):
         self.x = x
         if (self.settings.board_x <= self.x and self.x < self.settings.board_x+self.settings.board_grid_width):
             return 0
@@ -50,13 +50,40 @@ class Board:
             return 6
 
     def create_piece(self, x):
-        self.column = self.get_column(x)
+        self.column = self._get_column(x)
         self.row = self.count_each_column[self.column]
         if self.row < self.settings.board_row:
             self.count_each_column[self.column] += 1
             self.pieces.append(Piece(self.column, self.row, self.player_num))
             self.player_num = 2 if self.player_num == 1 else 1
 
-    # def is_end(self):
-    #     for piece in self.pieces:
-            # piece.
+    def is_win(self):
+        last_piece = self.pieces[-1]
+        self.verticals = []
+        self.horizontals = []
+        self.obliques_pos = []
+        self.obliques_neg = []
+        for piece in self.pieces:
+            if piece.player_num == last_piece.player_num:
+                if piece.column == last_piece.column:
+                    self.verticals.append(piece.row)
+                if piece.row == last_piece.row:
+                    self.horizontals.append(piece.column)
+                if piece.column - piece.row == last_piece.column - last_piece.row:
+                    self.obliques_pos.apppend(piece.row)
+                if piece.column + piece.row == last_piece.column + last_piece.row:
+                    self.obliques_neg.append(piece.row)
+        return self._is_continuous_four(self.verticals) | self._is_continuous_four(self.horizontals) | self._is_continuous_four(self.obliques_pos) | self._is_continuous_four(self.obliques_neg)
+    
+    def _is_continuous_four(self, nums):
+        nums.sort()
+        count = 0
+        for i in range(len(nums) - 1):
+            if nums[i] - nums[i+1] == 1:
+                count += 1
+            else:
+                count = 0
+        if count == 3:
+            return True
+        else:
+            return False
